418b9f9fccb30bfdce2c54ae6976d3aa4e4c474a
joJoRed <paradoxboixd@gmail.com>
2025-08-02 09:58:58 +0800

    Initial Commit

---
+++ boby/bobyApp.swift
@@ -0,0 +1,17 @@
+//
+//  bobyApp.swift
+//  boby
+//
+//  Created by T Krobot on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct bobyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
